42edc89e854e2049221b45fb0423d54acae9e2eb
Christian Futterlieb <christian.futterlieb@agenturamwasser.ch>
2020-05-08 16:05:29 +0200

    [FEATURE] Add "Language Chooser" functionality
    With this feature it is possible to have a message displayed, when a
    user could display the page in his matched language. For this to work
    as expected, enableLanguageRedirection must be disabled (in
    SiteConfiguration).
    
    Resolves: #1

---
+++ Configuration/TCA/Overrides/sys_template.php
@@ -0,0 +1,12 @@
+<?php
+/*
+ * Copyright by Agentur am Wasser | Maeder & Partner AG
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
+ */
+
+// Add the TypoScript for the "Language Chooser" feature
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile('language_matcher', 'Configuration/TypoScript/LanguageChooser/', 'Language-Chooser');
